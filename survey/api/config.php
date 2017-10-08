42f3d6421eac29d3693798080c255976f461da7e
Hamza Mirza <hamzamirza_156@hotmail.com>
2017-10-08 15:24:32 +0500

    first commit

---
+++ survey/api/config.php
@@ -0,0 +1,9 @@
+
+<?php
+		 $connection = mysqli_connect("sql202.byetcluster.com", "epiz_20806876", "vEBz7BFXBT", "epiz_20806876_survey");
+		 if (mysqli_connect_errno())
+		  {
+		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		  die();
+		  }
+?>
