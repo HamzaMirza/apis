9fc2d529237176856bf3fe6c7aba9321b93c816f
HamzaMirza <hamzamirza_156@hotmail.com>
2018-01-09 15:07:31 +0500

    update

--- survey/api/config.php
+++ survey/api/config.php
@@ -1,6 +1,5 @@
-
 <?php
-		 $connection = mysqli_connect("sql202.byetcluster.com", "epiz_20806876", "vEBz7BFXBT", "epiz_20806876_survey");
+		 $connection = mysqli_connect("localhost","lotfashi_raza","V3ryS!mpl3","lotfashi_survey");
 		 if (mysqli_connect_errno())
 		  {
 		  echo "Failed to connect to MySQL: " . mysqli_connect_error();
